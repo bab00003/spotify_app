95368c71c0cda0bba932cc9cf357068ac2a8e4c3
Brock Brillhart <bab00003@mix.wvu.edu>
2023-05-26 11:05:34 -0400

    Created Playlist model and card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { LayoutMainComponent } from './layout/layout-main.component';
 import { YourLibraryComponent } from './body/your-library.component';
 import { SearchPageComponent } from './body/search-page.component';
 import { AppRoutingModule } from './app-routing.module';
+import { PlaylistCardComponent } from './body/playlist-card.component';
 
 @NgModule({
   declarations: [
@@ -28,7 +29,8 @@ import { AppRoutingModule } from './app-routing.module';
     BodyComponent,
     LayoutMainComponent,
     YourLibraryComponent,
-    SearchPageComponent
+    SearchPageComponent,
+    PlaylistCardComponent
   ],
   imports: [
     BrowserModule,
