513686a13aa9448987162b17b2d3bfb3485845f6
Brock Brillhart <bab00003@mix.wvu.edu>
2023-06-13 10:46:14 -0400

    Updated Playlist lists

--- src/app/body/mock-playlists.ts
+++ src/app/body/mock-playlists.ts
@@ -1,22 +1,22 @@
 export const mock_playlists = [
-{
-    title: "Liked Songs",
-    author: "Username",
-    imagePath: "./assets/likedsongs.jpg"
-},
-{
-    title: "Workout Playlist",
-    author: "Username",
-    imagePath: "./assets/workout.png"
-},
-{
-    title: "Car Playlist",
-    author: "Username",
-    imagePath: "./assets/car.jpg"
-},
-{
-    title: "Study Beats",
-    author: "Usernae",
-    imagePath: "./assets/book.png"
-},
+    {
+        title: "Liked Songs",
+        author: "Username",
+        imagePath: "./assets/likedsongs.jpg"
+    },
+    {
+        title: "Workout Playlist",
+        author: "Username",
+        imagePath: "./assets/workout.png"
+    },
+    {
+        title: "Car Playlist",
+        author: "Username",
+        imagePath: "./assets/car.jpg"
+    },
+    {
+        title: "Study Beats",
+        author: "Usernae",
+        imagePath: "./assets/book.png"
+    },
 ]
--- src/app/body/your-library.component.ts
+++ src/app/body/your-library.component.ts
@@ -2,6 +2,28 @@ import { Component } from "@angular/core";
 import { Playlist } from "./playlist.model";
 import { mock_playlists } from "./mock-playlists";
 
+var data = {
+    playlist1: {
+        title: "Liked Songs",
+        author: "Username",
+        imagePath: "./assets/likedsongs.jpg"
+    },
+    playlist2: {
+        title: "Workout Playlist",
+        author: "Username",
+        imagePath: "./assets/workout.png"
+    },
+    playlist3: {
+        title: "Car Playlist",
+        author: "Username",
+        imagePath: "./assets/car.jpg"
+    },
+    playlist4: {
+        title: "Study Beats",
+        author: "Usernae",
+        imagePath: "./assets/book.png"
+    },
+}
 @Component({
     selector: "spotify-your-library",
     templateUrl: "your-library.component.html"
@@ -9,6 +31,10 @@ import { mock_playlists } from "./mock-playlists";
 export class YourLibraryComponent {
     playlists: Playlist[] = [];
     constructor() {
+        console.log(data);
+        for (var item in data) {
+            console.log(item)
+        }
         for (var playlist of mock_playlists) {
             this.playlists.push(new Playlist(playlist));
         }
