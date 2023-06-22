e66d7e40ca2683a26a819a544abb0d82d5dadd2f
Brock Brillhart <bab00003@mix.wvu.edu>
2023-06-22 11:31:11 -0400

    Added user model

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -2,6 +2,8 @@ import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { environment } from "src/environments/environment";
 import { AuthResponse } from "./authResponse";
+import { Subject, tap } from "rxjs";
+import { User } from "./user.model";
 
 @Injectable({
     providedIn: 'root'
@@ -10,6 +12,7 @@ export class AuthService {
     baseUrl: string = "https://identitytoolkit.googleapis.com/v1/accounts";
     signUpEndpoint: string = "signUp";
     signInEndpoint: string = "signInWithPassword";
+    private user: Subject<User> = new Subject<User>();
 
     public constructor(private http: HttpClient) {
 
@@ -21,7 +24,7 @@ export class AuthService {
             "returnSecureToken": true
         };
         return this.http.post<AuthResponse>(this.baseUrl + ':' + this.signUpEndpoint + '?' +
-                                            'keys' + environment.firebase.apiKey, requestBody);
+            'keys' + environment.firebase.apiKey, requestBody);
     }
 
     public login(email: string, password: string) {
@@ -31,7 +34,17 @@ export class AuthService {
             "returnSecureToken": true
         };
 
-        return this.http.post<AuthResponse>(this.baseUrl + ':' + this.signInEndpoint + '?' + 
-                                            'keys' + environment.firebase.apiKey, requestBody);
+        return this.http.post<AuthResponse>(this.baseUrl + ':' + this.signInEndpoint + '?' +
+            'keys' + environment.firebase.apiKey, requestBody).pipe(
+                tap(
+                    (data: AuthResponse) => {
+                        const currentTime = new Date().getTime();
+                        const expirationDate = new Date(currentTime + +data.expiresIn * 1000);
+                        const user = new User(data.email, data.localId, data.idToken, expirationDate);
+                        this.user.next(user);
+                        console.log(user);
+                    }
+                )
+            );
     }
 }
